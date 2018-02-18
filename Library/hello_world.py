f318f923bd31a2f382986896711cfb381a1274b0
ShaneCornell <35901126+ShaneCornell@users.noreply.github.com>
2018-02-18 13:42:25 -0700

    added html template

--- Library/hello_world.py
+++ Library/hello_world.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/test')
 def hello_world():
     return 'hello world'
+
+@app.route('/')
+def whatever():
+    a = "round"
+    return render_template('index.html',shape = a)
